0c4d7d2b4e18e3adfd5dc90346615ae12741012e
Michael Bian <166888204+MMMMMaasd@users.noreply.github.com>
2024-10-27 07:53:34 +0800

    Update main.cpp

--- Project3/SDLSimple/main.cpp
+++ Project3/SDLSimple/main.cpp
@@ -1,7 +1,7 @@
 /**
 * Author: [Michael Bian]
 * Assignment: Lunar Lander
-* Date due: 2024-10-26, 11:59pm
+* Date due: 2024-10-27, 11:59pm
 * I pledge that I have completed this assignment without
 * collaborating with anyone else, in conformance with the
 * NYU School of Engineering Policies and Procedures on
